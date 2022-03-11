824b0822edc01a0674f6359fed1e2d3432062b52
Sergey Tkachenko <tkachenkosi@gmail.com>
2022-03-11 10:35:42 +0300

    Исправления по замечаниям

--- js/card.js
+++ js/card.js
@@ -8,23 +8,29 @@ const setFieldTmp = (fieldTmp, valueData) => {
   }
 };
 
-const houseType = {
-  flat: 'Квартира',
-  bungalow: 'Бунгало',
-  house: 'Дом',
-  palace: 'Дворец',
-  hotel: 'Отель',
+const HouseType = {
+  FLAT: 'Квартира',
+  BUNGALOW: 'Бунгало',
+  HOUSE: 'Дом',
+  PALACE: 'Дворец',
+  HOTEL: 'Отель',
 };
 
 const createCard = (data) => {
   const cardTemplate = document.querySelector('#card').content.querySelector('.popup').cloneNode(true);
   const cardFeatures = cardTemplate.querySelector('.popup__features');
   const cardPhotos = cardTemplate.querySelector('.popup__photos');
+  const cardAvatar = cardTemplate.querySelector('.popup__avatar');
 
+  if (data.author.avatar) {
+    cardAvatar.src = data.author.avatar;
+  } else {
+    cardAvatar.classList.add(HIDDEN_CLASS);
+  }
   setFieldTmp(cardTemplate.querySelector('.popup__title'), data.offer.title);
   setFieldTmp(cardTemplate.querySelector('.popup__text--address'), data.offer.address);
   setFieldTmp(cardTemplate.querySelector('.popup__text--price'), `${data.offer.price} ₽/ночь`);
-  setFieldTmp(cardTemplate.querySelector('.popup__type'), houseType[data.offer.type]);
+  setFieldTmp(cardTemplate.querySelector('.popup__type'), HouseType[data.offer.type]);
   setFieldTmp(cardTemplate.querySelector('.popup__text--capacity'), `${data.offer.rooms} комнаты для ${data.offer.guests} гостей`);
   setFieldTmp(cardTemplate.querySelector('.popup__text--time'), `Заезд после ${data.offer.checkin}, выезд до ${data.offer.checkout}`);
   setFieldTmp(cardTemplate.querySelector('.popup__description'), data.offer.description);
