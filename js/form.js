45a12253f6ba5233cf4e05daf1aedf94723ae57e
Sergey Tkachenko <tkachenkosi@gmail.com>
2022-03-17 10:52:11 +0300

    Настройка валидации

--- js/form.js
+++ js/form.js
@@ -15,5 +15,52 @@ const toggleActivateForm = (shouldActivate) => {
   }
 };
 
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  successClass: 'ad-form__element--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+}, false);
+
+
+function validateTitle(value) {
+  return value.length >= 30 && value.length <= 100;
+}
+
+function validatePrice(value) {
+  return value >= 1 && value <= 100000;
+}
+
+pristine.addValidator(adForm.querySelector('#title'), validateTitle, 'Длина поля от 30 до 100 символов');
+pristine.addValidator(adForm.querySelector('#price'), validatePrice, 'Максимальная сумма 100000');
+
+
+const fieldRoom = adForm.querySelector('#room_number');
+const fieldCapacity = adForm.querySelector('#capacity');
+const optionCaoacity = {
+  1 : ['1',],
+  2 : ['1','2'],
+  3 : ['1','2','3'],
+  100 : ['0',],
+};
+
+function validateRoom() {
+  return optionCaoacity[fieldRoom.value].includes(fieldCapacity.value);
+}
+
+pristine.addValidator(fieldRoom, validateRoom, `Количество комнат ${fieldRoom.value} не соответствует числу гостей ${fieldCapacity.value}`);
+pristine.addValidator(fieldCapacity, validateRoom, `Число гостей ${fieldCapacity.value} не соответствует количеству комнат ${fieldRoom.value}`);
+
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+
+});
+
+
 export {toggleActivateForm};
 
