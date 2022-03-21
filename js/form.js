36bb734b37f557a86e9172e2f9a34929e6677f18
Sergey Tkachenko <tkachenkosi@gmail.com>
2022-03-21 04:21:27 +0300

    Добавил в форму слайдер для поля цены

--- js/form.js
+++ js/form.js
@@ -3,6 +3,7 @@ const fields = adForm.children;
 const filtersForm = document.querySelector('.map__filters');
 const filters = filtersForm.children;
 
+
 const toggleActivateForm = (shouldActivate) => {
   adForm.classList[shouldActivate ? 'remove' : 'add']('ad-form--disabled');
   filtersForm.classList[shouldActivate ? 'remove' : 'add']('map__filters--disabled');
@@ -25,21 +26,74 @@ const pristine = new Pristine(adForm, {
   errorTextClass: 'form__error'
 }, false);
 
+const MAX_PRICE = 100000;
+
+const fieldPrice = adForm.querySelector('#price');
+const fieldType = adForm.querySelector('#type');
+const fieldRoom = adForm.querySelector('#room_number');
+const fieldCapacity = adForm.querySelector('#capacity');
+
+const elementSlider = adForm.querySelector('.ad-form__slider');
+
+
+const MinPriceForType = {
+  BUNGALOW : '0',
+  FLAT : '1000',
+  HOTEL : '3000',
+  HOUSE : '5000',
+  PALACE : '10000',
+};
+
+const getMinPrice = () => Number(MinPriceForType[adForm.querySelector('#type').value.toUpperCase()]);
+
+
+noUiSlider.create(elementSlider, {
+  range: {
+    min: getMinPrice(),
+    max: MAX_PRICE,
+  },
+  start: 0,
+  step: 1,
+  connect: 'lower',
+});
+
+elementSlider.noUiSlider.on('update', () => {
+  fieldPrice.value = elementSlider.noUiSlider.get();
+});
+
 
 function validateTitle(value) {
   return value.length >= 30 && value.length <= 100;
 }
 
+fieldType.addEventListener('change', (evt) => {
+  const price = MinPriceForType[evt.target.value.toUpperCase()];
+  elementSlider.noUiSlider.updateOptions({
+    range: {
+      min: Number(price),
+      max: MAX_PRICE,
+    },
+    start: 0,
+  });
+
+  fieldPrice.placeholder = price;
+});
+
+
+fieldPrice.addEventListener('change', (evt) => {
+  elementSlider.noUiSlider.updateOptions({
+    start: evt.target.value,
+  });
+});
+
 function validatePrice(value) {
-  return value >= 1 && value <= 100000;
+  return value >= getMinPrice() && value <= 100000;
 }
 
 pristine.addValidator(adForm.querySelector('#title'), validateTitle, 'Длина поля от 30 до 100 символов');
-pristine.addValidator(adForm.querySelector('#price'), validatePrice, 'Максимальная сумма 100000');
+pristine.addValidator(fieldPrice, validatePrice, `Минимальная сумма ${getMinPrice()}, максимальная 100000`);
 
 
-const fieldRoom = adForm.querySelector('#room_number');
-const fieldCapacity = adForm.querySelector('#capacity');
 const optionCaoacity = {
   1 : ['1',],
   2 : ['1','2'],
